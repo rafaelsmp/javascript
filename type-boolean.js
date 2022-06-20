dd63c24b37dad5fbf1b5e72b28a9d5f878f083db
rafaelsmp <86517878+rafaelsmp@users.noreply.github.com>
2022-06-19 22:28:03 -0300

    Adicionando arquivos JavaScript

---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+//Tipo Boolean
+//true -> verdadeiro
+//false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+//const segundoNumero = 10;
+let cadastroAtivado = false;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
+
+
---
+++ type-string.js
@@ -0,0 +1,23 @@
+//Tipo String
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const stringDeNumeros = "34567";
+
+
+
+//Concatenação (+)
+
+const citacao = "Meu nome é: ";
+const meuNome = "Rafael";
+console.log(citacao + meuNome)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
