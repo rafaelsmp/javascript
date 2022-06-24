ab58c4e38bc952c13e4e748402891cd820a06658
rafaelsmp <86517878+rafaelsmp@users.noreply.github.com>
2022-06-23 23:15:32 -0300

    Atualização Curso JS - 23/06/2022

---
+++ console.js
@@ -0,0 +1,16 @@
+//log -> registro
+
+const minhaVar = true;
+
+console.log(574)
+console.log("Eu sou um texto")
+console.log(minhaVar)
+
+//tratamento de erro!
+console.error('Deu erro!!') 
+console.error(new Error("deu erro"));
+
+//conso/le.error() para exibir mensagens de erro;
+//console.table() para visualizar de forma mais organizada informações tabulares;
+//console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+//console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
---
+++ erros.js
@@ -0,0 +1,6 @@
+const numero = 0;
+//SyntaxError --> const numero;
+
+//SyntaxError --> console.log(numero
+
+//ReferenceError: minhaVar is not defined --> console.log(minhaVar)
